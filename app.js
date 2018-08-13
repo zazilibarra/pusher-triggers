2c18a85c2ace7d2da470db15242377aeaae1fa14
imorales-logistikgo <imorales@logistikgo.com>
2018-08-13 14:31:58 -0500

    Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ var pusher = new Pusher({
   encrypted: true
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/',(req, res)=>{
 	res.send(`API PUSHER TRIGGERS LOGISTIKGO VERSIÓN:${process.env.npm_package_version}`);
